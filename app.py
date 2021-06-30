445f7b617cf1d57f332a7502575e7e51f278414f
Kristian Roopnarine <kristian.roopnarine@gmail.com>
2021-06-30 19:38:01 -0400

    Final : finished logic to move files into proper directories

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from os.path import splitext
 from pathlib import Path
+import shutil
 
 """
 What do I want to do
@@ -13,7 +14,6 @@ Iterate over the files and move them according to their extension.
 
 DOWNLOAD_DIR = f'{Path.home()}/Downloads'
 
-objects_in_download_folder = os.listdir(DOWNLOAD_DIR)
 
 def is_file(file):
     return os.path.isfile(f'{DOWNLOAD_DIR}/{file}')
@@ -31,9 +31,43 @@ def get_unique_file_extensions(files):
             # some files do not have extensions
             if file_ext not in extension_dict and file_ext != '':
                 extension_dict[file_ext] = file_ext + suffix
+    return extension_dict
             
-    print(extension_dict)
-                
-get_unique_file_extensions(objects_in_download_folder)
+
+def create_directories(ext_dict):
+    for dir_name in ext_dict.values():
+        extension_dir = f'{DOWNLOAD_DIR}/{dir_name}'
+        if not os.path.isdir(extension_dir):
+            os.mkdir(extension_dir)
+
+def move_files_to_directory(download_dir_objs,ext_dict):
+    for obj in download_dir_objs:
+        curr_dir = f'{DOWNLOAD_DIR}/{obj}'
+        file_ext = None
+        extension_dir = None
+
+        if is_file(obj):
+            file_ext = get_file_extension(obj)
+
+        # some files don't have extension
+        # some objects are directories
+        if file_ext == '' or file_ext is None:
+            extension_dir = ext_dict['unknown']
+        else:
+            extension_dir = ext_dict[file_ext]
+        
+        final_dir = f'{DOWNLOAD_DIR}/{extension_dir}/{obj}'
+
+        shutil.move(curr_dir,final_dir)
+            
+            
+
+
+if __name__ == '__main__':
+    objects_in_download_folder = os.listdir(DOWNLOAD_DIR)
+    ext_dict = get_unique_file_extensions(objects_in_download_folder)
+
+    create_directories(ext_dict)
+    move_files_to_directory(objects_in_download_folder, ext_dict)
 
 
